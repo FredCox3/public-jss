6a7f3511dd5cb3ad5368015f20085b3e0d332279
FredCox3 <freddie@coxtn.com>
2015-07-21 14:31:30 -0400

    initial commit

---
+++ Swift-Groups/JSS Buildings/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSS Buildings
+//
+//  Created by freddiecox on 7/11/15.
+//  Copyright © 2015 freddiecox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
